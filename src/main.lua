66a56c063260dbe08768e23607a40da4eaee1ec5
Lord Niels N. Horn <i.am@nilq.dk>
2017-02-08 21:36:48 +0100

    Add hyper graphics bindings

--- src/main.lua
+++ src/main.lua
@@ -1,4 +1,4 @@
-thing = require("project/project")
+thing = require("project/")
 local a = 0
 do
   local _with_0 = love
@@ -13,67 +13,22 @@ do
     a = a + dt
     point[1] = 100 * math.cos(a)
     point[2] = 100 * math.sin(a)
+    point[3] = 100 * -math.cos(a)
   end
   _with_0.draw = function()
-    local p, size = thing.project_to(2, 250, point[1], 100, 100, 20, point[2])
-    _with_0.graphics.circle("fill", 200 + p[1], 200 + p[2], 10 * size)
-    _with_0.graphics.setColor(0, 0, 255)
-    thing.draw_line(250, {
-      point[1] - 100,
-      -200,
-      0
-    }, {
-      point[1] - 100,
-      -200,
-      0
-    })
-    _with_0.graphics.setColor(0, 255, 255)
-    thing.draw_line(250, {
-      200,
-      point[1] - 100,
-      0
-    }, {
-      -200,
-      point[1] - 100,
-      0
-    })
     _with_0.graphics.setColor(0, 255, 0)
-    thing.draw_line(250, {
-      200,
-      0,
-      point[1]
-    }, {
-      -200,
-      0,
-      point[1]
-    })
-    _with_0.graphics.setColor(255, 0, 0)
-    thing.draw_line(250, {
-      200,
-      100,
-      100,
+    return thing.graphics.triangle(250, "fill", {
       point[1],
-      0
-    }, {
-      -200,
-      100,
-      100,
-      point[1],
-      100
-    })
-    _with_0.graphics.setColor(255, 255, 0)
-    return thing.draw_line(250, {
       200,
-      300,
-      100,
-      0,
-      point[1]
+      200
     }, {
-      -200,
-      300,
-      100,
-      100,
-      point[1]
+      200,
+      point[2],
+      200
+    }, {
+      200,
+      200,
+      point[3]
     })
   end
   return _with_0
--- src/main.moon
+++ src/main.moon
@@ -1,4 +1,4 @@
-export thing = require "project/project"
+export thing = require "project/"
 
 a = 0
 
@@ -15,23 +15,8 @@ with love
 
     point[1] = 100 * math.cos a
     point[2] = 100 * math.sin a
+    point[3] = 100 * -math.cos a
 
   .draw = ->
-    p, size = thing.project_to 2, 250, point[1], 100, 100, 20, point[2]
-
-    .graphics.circle "fill", 200 + p[1], 200 + p[2], 10 * size
-
-    .graphics.setColor 0, 0, 255
-    thing.draw_line 250, {point[1] - 100, -200, 0}, {point[1] - 100, -200, 0}
-
-    .graphics.setColor 0, 255, 255
-    thing.draw_line 250, {200, point[1] - 100, 0}, {-200, point[1] - 100, 0}
-
     .graphics.setColor 0, 255, 0
-    thing.draw_line 250, {200, 0, point[1]}, {-200, 0, point[1]}
-
-    .graphics.setColor 255, 0, 0
-    thing.draw_line 250, {200, 100, 100, point[1], 0}, {-200, 100, 100, point[1], 100}
-
-    .graphics.setColor 255, 255, 0
-    thing.draw_line 250, {200, 300, 100, 0, point[1]}, {-200, 300, 100, 100, point[1]}
+    thing.graphics.triangle 250, "fill", {point[1], 200, 200}, {200, point[2], 200}, {200, 200, point[3]}
